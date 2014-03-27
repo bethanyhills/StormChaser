5fc572d82e07261b3d99b8a3658b79d29f55cb8d
vagrant <vagrant@student-dev-box>
2014-03-27 20:47:00 +0000

    Initial commit. Set up rails app, installed rspec, testing gems, and postgres

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+StormChaser::Application.config.secret_key_base = '227c71cac8b0b4fc2c74bf2a2f05fc70ad720c905ba5b078503247f0af86ea5e99835f9d97d0ec9e93e1c771409b3c758316883127c502b0f76dabaed69e75c7'
