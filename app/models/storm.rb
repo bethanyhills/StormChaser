261fa17183c32cd1e47afb28ee2377cf3af456a6
Patrick Brennan <chief125@gmail.com>
2014-03-31 15:24:44 -0500

    Changed the index_map method to sort by strength and provide the 500 strongest tornadoes available. Added f_scale to the data that is provided by many_storm_map_data Storm model method for color coding of strength on the map

--- app/models/storm.rb
+++ app/models/storm.rb
@@ -3,11 +3,11 @@ class Storm < ActiveRecord::Base
   belongs_to :path
 
   def self.many_storm_map_data()
-    self.select("start_lat", "start_long", "stop_lat", "stop_long", "storms.id")
+    self.select("start_lat", "start_long", "stop_lat", "stop_long", "storms.id", "f_scale")
   end
 
   def self.index_map()
-    Storm.joins(:path).where("paths.complete_track").limit(500).many_storm_map_data
+    Storm.joins(:path).where("paths.complete_track").order(f_scale: :desc).limit(500).many_storm_map_data
   end
 
 end
