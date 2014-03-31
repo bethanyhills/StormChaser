76fd63c671c4e8b5b3bea2ff3606a92e45aa6242
vagrant <vagrant@student-dev-box>
2014-03-31 19:45:00 +0000

    Add user spec set up and code to test storm#index_map method

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :user do
+  factory :user do |u|
+	u.name "test"
+	u.sequence(:email) { |n| "mike#{n}@email.com"}
+	u.password "password"
+	end
   end
 end
--- spec/models/storm_spec.rb
+++ spec/models/storm_spec.rb
@@ -8,19 +8,27 @@ describe Storm do
 	before(:each) do 
 		StormChaser::Application.load_tasks
 		Rake::Task.define_task(:environment)
-		# Rake::Task['db:drop'].invoke
-		# Rake::Task['db:create'].invoke
-		# Rake::Task['db:migrate'].invoke
 		Rake::Task['import:only_5s'].invoke
-		@storm = Storm.all.limit(10).many_storm_map_data()
+		
 	end
 
 describe "#many_storm_map_data" do
 	it "returns an array of objects holding tornado data" do
-
+	@storm = Storm.all.limit(10).many_storm_map_data()
+   
    expect(@storm.first.start_lat).to_not be_nil
    expect {@storm.first.width}.to raise_error
    expect(@storm.length).to be(10)
 	end
+end
+
+describe "#index_map" do 
+	it "returns all storms with a complete path " do
+	@storm = Storm.index_map
+	@bad_storm = Storm.joins(:path).where("!paths.complete_track").first
+
+   expect(@storm.find(@bad_storm.id)).to be_empty
+	end
+	
 end
 end
