d928cdef4a832fb174dd8a129ed5314497d2c375
vagrant <vagrant@student-dev-box>
2014-03-31 16:29:21 +0000

    Add rspec test for Storms model

--- spec/models/storm_spec.rb
+++ spec/models/storm_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
+before(:each) do
+    @storm = Storm.all.limit(10)
+end
 
 describe Storm do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#many_storm_map_data" do
+  	it "returns an array of objects holding tornado data" do
+     @storm.many_storm_map_data()
+
+     expect(@storm.first.start_lat).to exist
+     expect(@storm.first.width).to_not exist
+     expect(@storm.length).to be(10)
+    end
+  end
 end
