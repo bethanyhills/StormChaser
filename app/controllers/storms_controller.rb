28b7790d2855fbc9b856c40758c22ab2b79f2655
Patrick Brennan <chief125@gmail.com>
2014-03-31 20:22:41 -0500

    Moved the logic for the historical data api call to the Storm model to method hist_weather_api from the Storm controller.

--- app/controllers/storms_controller.rb
+++ app/controllers/storms_controller.rb
@@ -5,35 +5,13 @@ class StormsController < ApplicationController
 
   def show
     @storm = Storm.find(params[:id])
-    @date = TornadoDate.find(@storm.tornado_date_id)
+    @date = @storm.tornado_date
     gon.storms = @storm
   end
 
   def hist_weather_api
-    require 'unirest'
-    storm = Storm.find(params[:id])
-    month = storm.tornado_date.month.to_s
-    month = '0' + month if month.length == 1
-    day = storm.tornado_date.day.to_s
-    day = '0' + day if day.length == 1
-    hour = storm.hour.to_s #16
-    hour = '0' + hour if hour.length == 1
-    minute = storm.minute.to_s #10
-    minute = '0' + minute if minute.length == 1
-    time = storm.tornado_date.year.to_s + '-' + month + '-' + day + 'T' + storm.hour.to_s + ':' + storm.minute.to_s + ':00-0600'
-    puts time
-
-    #2013-05-06T12:00:00-0400
-
-    response = Unirest.get('https://api.forecast.io/forecast/'+ENV['FORECAST_IO_KEY'].to_s+'/'+storm.start_lat.to_s+','+storm.start_long.to_s + ',' + time,
-      headers: { "Accept" => "application/json" })
-
-    location_data = response.body
-
     respond_to do |format|
-      format.html # show.html.erb
-      # format.xml  { render :xml => l }
-      format.json { render :json => location_data }
+      format.json { render :json => Storm.historical_data(params[:id]) }
     end
   end
 end
--- app/models/storm.rb
+++ app/models/storm.rb
@@ -10,4 +10,25 @@ class Storm < ActiveRecord::Base
     Storm.joins(:path).where("paths.complete_track").order(f_scale: :desc).limit(500).many_storm_map_data
   end
 
+  def self.historical_data(id)
+    require 'unirest'
+    storm = Storm.find(id)
+    month = storm.tornado_date.month.to_s
+    month = '0' + month if month.length == 1
+    day = storm.tornado_date.day.to_s
+    day = '0' + day if day.length == 1
+    hour = storm.hour.to_s #16
+    hour = '0' + hour if hour.length == 1
+    minute = storm.minute.to_s #10
+    minute = '0' + minute if minute.length == 1
+    time = storm.tornado_date.year.to_s + '-' + month + '-' + day + 'T' + storm.hour.to_s + ':' + storm.minute.to_s + ':00-0600'
+
+    #2013-05-06T12:00:00-0400
+
+    response = Unirest.get('https://api.forecast.io/forecast/'+ENV['FORECAST_IO_KEY'].to_s+'/'+storm.start_lat.to_s+','+storm.start_long.to_s + ',' + time,
+      headers: { "Accept" => "application/json" })
+
+    location_data = response.body
+  end
+
 end
