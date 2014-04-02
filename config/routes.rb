37027d2dbc3fd0242d90cd876005c5f23fcac54e
Bethany Nagel <beth0851@gmail.com>
2014-04-02 16:45:25 -0500

    cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,65 +1,9 @@
 StormChaser::Application.routes.draw do
   devise_for :users
   root "storms#index"
-    resources :users
-    get '/storms/radiussearch', to: 'storms#search_api_call'
-    get '/storms/:id/histweather', to: 'storms#hist_weather_api'
-    resources :storms
-    get '/search', to: 'storms#search_radius'
-
-  # get '/movies/gladiator', to: 'movies#show_gladiator'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  resources :users
+  get '/storms/radiussearch', to: 'storms#search_api_call'
+  get '/storms/:id/histweather', to: 'storms#hist_weather_api'
+  resources :storms
+  get '/search', to: 'storms#search_radius'
 end
