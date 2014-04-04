f85a859b85b76f0f2861e41546b13b669e283961
vagrant <vagrant@student-dev-box>
2014-04-04 15:34:39 +0000

    Switched main page to mapbox.  Added javascript to plot individual cyclones with the ability to click and be redirected to that individual storms dashboard.

--- app/assets/javascripts/cyclones.js
+++ app/assets/javascripts/cyclones.js
@@ -2,30 +2,30 @@
 var map = L.mapbox.map('map', 'bethanynagel.hmm5bk2l',
   {center: [38.5, -98.00], 
   zoom: 4,
-  doubleClickZoom: true,
   })
 
-//create empty array to hold markers
-markers = [];
 //create tornado icon
 var myIcon = L.icon({
   iconUrl: 'tornado-small.png'
   });
-var id = []
 
 // Construct the lat and long for each tornado.
 for (var i = 0; i < gon.cyclones.length; i++) {
   var start_lat = gon.cyclones[i]["start_lat"]
   var start_long = gon.cyclones[i]["start_long"]
-  id[i] = gon.cyclones[i]["id"]
-  
-  //add icon to map for this tornado
-  L.marker([start_lat, start_long], {icon: myIcon}).addTo(map).addEventListener('click', function(e) {
-    console.log(id[i])
-    //window.location.href = "/cyclones/" + this["id"];
-  });
+  var id = gon.cyclones[i]["id"]
+  var scale = gon.cyclones[i]["f_scale"]
+
+  // add icon to map for this tornado, bind popup, and set url to redirect to specific storm dashboard.
+L.marker([start_lat, start_long], {icon: myIcon, alt: id})
+  .bindPopup('<p>Category ' + scale + ' Tornado</p><a href="/cyclones/'+id+'">Chase this Storm!</a>')
+  .addTo(map)
 
 }//closes for loop
+
+
+
+
 
 
   
