10de26a53caf5448def3e69ea22ee8c33bf8db2c
vagrant <vagrant@student-dev-box>
2014-04-04 16:31:05 +0000

    Add leaflet plug-in and javascript to create clustering effect on front page

---
+++ app/assets/javascripts/cyclones-show.js
@@ -0,0 +1,21 @@
+// Create the map
+var map = L.mapbox.map('map', 'bethanynagel.hmm5bk2l',
+  {center: [38.5, -98.00], 
+  zoom: 4,
+  })
+
+//create tornado icon
+var myIcon = L.icon({
+  iconUrl: '../tornado-small.png'
+  });
+
+// Construct the lat and long for this tornado.
+  var start_lat = gon.cyclones["start_lat"]
+  var start_long = gon.cyclones["start_long"]
+  var id = gon.cyclones["id"]
+  var scale = gon.cyclones["f_scale"]
+
+  // add icon to map for this tornado, bind popup, and set url to redirect to specific storm dashboard.
+L.marker([start_lat, start_long], {icon: myIcon, alt: id})
+  .addTo(map)
+
--- app/assets/javascripts/cyclones.js
+++ app/assets/javascripts/cyclones.js
@@ -1,13 +1,13 @@
 // Create the map
-var map = L.mapbox.map('map', 'bethanynagel.hmm5bk2l',
-  {center: [38.5, -98.00], 
-  zoom: 4,
-  })
+var map = L.mapbox.map('map', 'bethanynagel.hmm5bk2l')
+  .setView([38.5, -98.00], 4);
 
 //create tornado icon
 var myIcon = L.icon({
   iconUrl: 'tornado-small.png'
   });
+
+var markers = new L.MarkerClusterGroup();
 
 // Construct the lat and long for each tornado.
 for (var i = 0; i < gon.cyclones.length; i++) {
@@ -17,11 +17,16 @@ for (var i = 0; i < gon.cyclones.length; i++) {
   var scale = gon.cyclones[i]["f_scale"]
 
   // add icon to map for this tornado, bind popup, and set url to redirect to specific storm dashboard.
-L.marker([start_lat, start_long], {icon: myIcon, alt: id})
-  .bindPopup('<p>Category ' + scale + ' Tornado</p><a href="/cyclones/'+id+'">Chase this Storm!</a>')
-  .addTo(map)
+  var marker = L.marker(new L.latLng(start_lat, start_long), {
+      icon: myIcon}
+  );
+  marker.bindPopup('<p>Category ' + scale + ' Tornado</p><a href="/cyclones/'+id+'">Chase this Storm!</a>');
+  markers.addLayer(marker);
 
 }//closes for loop
+
+map.addLayer(markers);
+
 
 
 
