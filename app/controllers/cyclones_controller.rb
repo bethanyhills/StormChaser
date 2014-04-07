3b3a8c003c3fa196ff931b6e52145e40cd05669f
Patrick Brennan <chief125@gmail.com>
2014-04-07 15:22:56 -0500

    Started converting the controller over to use the API

--- app/controllers/cyclones_controller.rb
+++ app/controllers/cyclones_controller.rb
@@ -1,17 +1,17 @@
 class CyclonesController < ApplicationController
   def index
-    gon.cyclones = Cyclone.index_map
+    # gon.cyclones = Cyclone.index_map
     # gon.cyclones = {'cyclones' => Cyclone.index_map, 'date' => CycloneDate.all}
   end
 
   def show
     @cyclone = Cyclone.find(params[:id])
-    gon.cyclones = @cyclone
+    # gon.cyclones = @cyclone
 
-    respond_to do |format|
-      format.html {}
-      format.json { render :json => @cyclone, status: 200 }
-    end
+    # respond_to do |format|
+    #   format.html {}
+    #   format.json { render :json => @cyclone, status: 200 }
+    # end
   end
 
   def hist_weather_api
@@ -21,7 +21,7 @@ class CyclonesController < ApplicationController
   end
 
   def search_radius
-    gon.cyclones = Cyclone.index_map
+    # gon.cyclones = Cyclone.index_map
   end
 
   def search_api_call
